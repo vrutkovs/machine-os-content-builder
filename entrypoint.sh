cf53db973ba42853f50153e592ade44df5264329
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-11-12 11:28:44 +0100

    Move /sbin binaries to /usr/sbin

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,6 +56,9 @@ pushd /tmp/working
     rpm2cpio $i | cpio -div
   done
   mv etc usr/
+  # /sbin is a symlink to /usr/sbin
+  mv sbin/* usr/sbin/
+  rm -rf sbin
 popd
 
 # add binaries from /srv/addons
